299411132b42420c9aba7bc0f40e746365be0e67
unknown <protexbeer41@gmail.com>
2025-11-10 13:35:13 +0600

    added error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router";
+import errorImg from "../assets/images/error-404.png";
 const ErrorPage = () => {
-  return <div>This error page</div>;
+  return (
+    <div className="flex flex-col justify-center items-center space-y-10 my-18">
+      <img src={errorImg} alt="" />
+      <h1 className="text-5xl text-[#00193199] font-bold">
+        Oops, page not found!
+      </h1>
+      <h3 className="text-lg text-[#00193199]">
+        The page your are looking for is not available.
+      </h3>
+      <Link
+        to="/"
+        className="btn w-[160px] h-11 text-lg bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white"
+      >
+        Go Back!
+      </Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
